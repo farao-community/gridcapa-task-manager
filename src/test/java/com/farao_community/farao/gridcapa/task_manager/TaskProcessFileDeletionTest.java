d346c1546cab4f20cee83ae8b2813be25fbf24c6
Sébastien Murgey <sebastien.murgey@rte-france.com>
2021-10-18 10:39:50 +0200

    Add copyright and author headers

--- src/test/java/com/farao_community/farao/gridcapa/task_manager/TaskProcessFileDeletionTest.java
+++ src/test/java/com/farao_community/farao/gridcapa/task_manager/TaskProcessFileDeletionTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.gridcapa.task_manager;
 
 import com.farao_community.farao.gridcapa.task_manager.entities.ProcessFile;
@@ -17,6 +23,9 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @author Sebastien Murgey {@literal <sebastien.murgey at rte-france.com>}
+ */
 @SpringBootTest
 class TaskProcessFileDeletionTest {
 
