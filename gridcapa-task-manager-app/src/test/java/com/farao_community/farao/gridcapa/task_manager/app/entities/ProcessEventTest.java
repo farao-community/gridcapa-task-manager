330b7fd88058e408050cb8d9855cf3ae09aaa8e8
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2021-12-15 15:16:30 +0100

    add unit test for entity conversion to Dto

---
+++ gridcapa-task-manager-app/src/test/java/com/farao_community/farao/gridcapa/task_manager/app/entities/ProcessEventTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.gridcapa.task_manager.app.entities;
+
+import com.farao_community.farao.gridcapa.task_manager.api.ProcessEventDto;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author Mohamed Benrejeb {@literal <mohamed.ben-rejeb at rte-france.com>}
+ */
+class ProcessEventTest {
+
+    @Test
+    void testConstructorFromEntity() {
+        Task task = Mockito.mock(Task.class);
+        ProcessEvent processEvent = new ProcessEvent(task, LocalDateTime.now(), "INFO", "CGM arrived");
+        ProcessEventDto processEventDto = ProcessEvent.createDtoFromEntity(processEvent);
+        assertEquals("INFO", processEventDto.getLevel());
+        assertNotNull(processEvent.getTimestamp());
+        assertEquals("CGM arrived", processEventDto.getMessage());
+    }
+}
