35c563700970df3c67d4ae0f3b7d5376612e7e46
walameni <walha.ameni@gmail.com>
2022-02-15 14:34:22 +0100

    remove formatting

--- gridcapa-task-manager-app/src/main/java/com/farao_community/farao/gridcapa/task_manager/app/TaskManager.java
+++ gridcapa-task-manager-app/src/main/java/com/farao_community/farao/gridcapa/task_manager/app/TaskManager.java
@@ -63,8 +63,8 @@ public class TaskManager {
     @Bean
     public Consumer<Flux<TaskStatusUpdate>> consumeTaskStatusUpdate() {
         return f -> f
-                .onErrorContinue((t, r) -> LOGGER.error(t.getMessage(), t))
-                .subscribe(this::handleTaskStatusUpdate);
+            .onErrorContinue((t, r) -> LOGGER.error(t.getMessage(), t))
+            .subscribe(this::handleTaskStatusUpdate);
     }
 
     public void handleTaskStatusUpdate(TaskStatusUpdate taskStatusUpdate) {
@@ -82,8 +82,8 @@ public class TaskManager {
     @Bean
     public Consumer<Flux<String>> consumeTaskEventUpdate() {
         return f -> f
-                .onErrorContinue((t, r) -> LOGGER.error(t.getMessage(), t))
-                .subscribe(this::handleTaskEventUpdate);
+            .onErrorContinue((t, r) -> LOGGER.error(t.getMessage(), t))
+            .subscribe(this::handleTaskEventUpdate);
     }
 
     void handleTaskEventUpdate(String loggerEventString) {
@@ -109,8 +109,8 @@ public class TaskManager {
     @Bean
     public Consumer<Flux<NotificationRecords>> consumeMinioEvent() {
         return f -> f
-                .onErrorContinue((t, r) -> LOGGER.error(t.getMessage(), t))
-                .subscribe(this::handleMinioEvent);
+            .onErrorContinue((t, r) -> LOGGER.error(t.getMessage(), t))
+            .subscribe(this::handleMinioEvent);
     }
 
     public void handleMinioEvent(NotificationRecords notificationRecords) {
@@ -147,11 +147,11 @@ public class TaskManager {
                 LOGGER.info("Adding MinIO object {}", objectKey);
                 String[] interval = validityInterval.split("/");
                 ProcessFileArrival processFileArrival = getProcessFileArrival(
-                        OffsetDateTime.parse(interval[0]),
-                        OffsetDateTime.parse(interval[1]),
-                        event,
-                        objectKey,
-                        fileType);
+                    OffsetDateTime.parse(interval[0]),
+                    OffsetDateTime.parse(interval[1]),
+                    event,
+                    objectKey,
+                    fileType);
                 processFileRepository.save(processFileArrival.processFile);
                 saveAndNotifyTasks(addProcessFileToTasks(processFileArrival.processFile, processFileArrival.fileEventType));
                 LOGGER.info("Process file {} has been added properly", processFileArrival.processFile.getFilename());
@@ -214,33 +214,33 @@ public class TaskManager {
     private Set<Task> addProcessFileToTasks(ProcessFile processFile, FileEventType fileEventType) {
         LOGGER.debug("Adding process file to the related tasks");
         return Stream.iterate(processFile.getStartingAvailabilityDate(), time -> time.plusHours(1))
-                .limit(ChronoUnit.HOURS.between(processFile.getStartingAvailabilityDate(), processFile.getEndingAvailabilityDate()))
-                .parallel()
-                .map(timestamp -> {
-                    Task task = taskRepository.findByTimestamp(timestamp).orElseGet(() -> new Task(timestamp));
-                    addFileEventToTask(task, fileEventType, processFile);
-                    task.addProcessFile(processFile);
-                    checkAndUpdateTaskStatus(task);
-                    return task;
-                })
-                .collect(Collectors.toSet());
+            .limit(ChronoUnit.HOURS.between(processFile.getStartingAvailabilityDate(), processFile.getEndingAvailabilityDate()))
+            .parallel()
+            .map(timestamp -> {
+                Task task = taskRepository.findByTimestamp(timestamp).orElseGet(() -> new Task(timestamp));
+                addFileEventToTask(task, fileEventType, processFile);
+                task.addProcessFile(processFile);
+                checkAndUpdateTaskStatus(task);
+                return task;
+            })
+            .collect(Collectors.toSet());
     }
 
     private Set<Task> removeProcessFileFromTasks(ProcessFile processFile) {
         LOGGER.debug("Removing process file of the related tasks");
         return taskRepository.findAllByTimestampBetween(processFile.getStartingAvailabilityDate(), processFile.getEndingAvailabilityDate())
-                .parallelStream()
-                .map(task -> {
-                    task.removeProcessFile(processFile);
-                    checkAndUpdateTaskStatus(task);
-                    if (task.getProcessFiles().isEmpty()) {
-                        task.getProcessEvents().clear();
-                    } else {
-                        addFileEventToTask(task, FileEventType.DELETED, processFile);
-                    }
-                    return task;
-                })
-                .collect(Collectors.toSet());
+            .parallelStream()
+            .map(task -> {
+                task.removeProcessFile(processFile);
+                checkAndUpdateTaskStatus(task);
+                if (task.getProcessFiles().isEmpty()) {
+                    task.getProcessEvents().clear();
+                } else {
+                    addFileEventToTask(task, FileEventType.DELETED, processFile);
+                }
+                return task;
+            })
+            .collect(Collectors.toSet());
     }
 
     private void checkAndUpdateTaskStatus(Task task) {
