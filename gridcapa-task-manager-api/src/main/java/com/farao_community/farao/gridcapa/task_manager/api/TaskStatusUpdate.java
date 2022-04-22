84d3f1eb989cd73f72a0e252f381a69cb509f110
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2022-04-22 12:18:33 +0200

    revert add timestamp to task updated message DTO

--- gridcapa-task-manager-api/src/main/java/com/farao_community/farao/gridcapa/task_manager/api/TaskStatusUpdate.java
+++ gridcapa-task-manager-api/src/main/java/com/farao_community/farao/gridcapa/task_manager/api/TaskStatusUpdate.java
@@ -9,7 +9,6 @@ package com.farao_community.farao.gridcapa.task_manager.api;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.time.OffsetDateTime;
 import java.util.UUID;
 
 /**
@@ -17,24 +16,17 @@ import java.util.UUID;
  */
 public class TaskStatusUpdate {
     private final UUID id;
-    private final OffsetDateTime timestamp;
     private final TaskStatus taskStatus;
 
     @JsonCreator
     public TaskStatusUpdate(@JsonProperty("id") UUID id,
-                            @JsonProperty("timestamp") OffsetDateTime timestamp,
                             @JsonProperty("taskStatus") TaskStatus taskStatus) {
         this.id = id;
-        this.timestamp = timestamp;
         this.taskStatus = taskStatus;
     }
 
     public UUID getId() {
         return id;
-    }
-
-    public OffsetDateTime getTimestamp() {
-        return timestamp;
     }
 
     public TaskStatus getTaskStatus() {
