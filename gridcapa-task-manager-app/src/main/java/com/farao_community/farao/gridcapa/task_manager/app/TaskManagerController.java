210b6e35de138e760af5071c0dcd013501a21487
Theo Pascoli <theo.pascoli@rte-france.com>
2023-01-30 10:28:05 +0100

    Now save object key and no more presigned url in database

--- gridcapa-task-manager-app/src/main/java/com/farao_community/farao/gridcapa/task_manager/app/TaskManagerController.java
+++ gridcapa-task-manager-app/src/main/java/com/farao_community/farao/gridcapa/task_manager/app/TaskManagerController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -113,7 +113,7 @@ public class TaskManagerController {
         allFiles.addAll(task.getOutputs());
         Optional<ProcessFileDto> myFile = allFiles.stream().filter(f -> f.getFileType().equals(fileType)).findFirst();
         if (myFile.isPresent()) {
-            BufferedInputStream in = new BufferedInputStream(this.fileManager.openUrlStream(myFile.get().getFileUrl()));
+            BufferedInputStream in = new BufferedInputStream(this.fileManager.openUrlStream(fileManager.generatePresignedUrl(myFile.get().getFileUrl())));
             result = ResponseEntity.ok()
                     .contentType(MediaType.APPLICATION_OCTET_STREAM)
                     .header(CONTENT_DISPOSITION, "attachment;filename=\"" + myFile.get().getFilename() + "\"")
