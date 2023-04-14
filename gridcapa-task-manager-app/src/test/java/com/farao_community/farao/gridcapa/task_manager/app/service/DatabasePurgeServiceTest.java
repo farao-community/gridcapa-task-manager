e01edc6a086d5c797bfa915cf12066fb34ceb61a
jipea <arnould.jp@gmail.com>
2023-04-14 11:12:57 +0200

    trailing space

--- gridcapa-task-manager-app/src/test/java/com/farao_community/farao/gridcapa/task_manager/app/service/DatabasePurgeServiceTest.java
+++ gridcapa-task-manager-app/src/test/java/com/farao_community/farao/gridcapa/task_manager/app/service/DatabasePurgeServiceTest.java
@@ -37,7 +37,7 @@ public class DatabasePurgeServiceTest {
 
     @Autowired
     private EventHandler eventHandler;
-    
+
     @Test
     void scheduledDatabaseTaskEventsPurgeTest() {
         OffsetDateTime offsetDateTimeNow = OffsetDateTime.now(ZoneId.of("UTC"));
