272cc4fe8c7f1312caa12f32e642148cdee47f14
Vincent BOCHET <vincent.bochet@rte-france.com>
2024-04-25 17:28:13 +0200

    Fix inversion of availableInputs and outputs in TaskDto

--- gridcapa-task-manager-app/src/main/java/com/farao_community/farao/gridcapa/task_manager/app/service/TaskDtoBuilderService.java
+++ gridcapa-task-manager-app/src/main/java/com/farao_community/farao/gridcapa/task_manager/app/service/TaskDtoBuilderService.java
@@ -132,8 +132,8 @@ public class TaskDtoBuilderService {
                 task.getTimestamp(),
                 task.getStatus(),
                 inputs,
-                outputs,
                 availableInputs,
+                outputs,
                 processEvents,
                 taskParameterDtos);
     }
