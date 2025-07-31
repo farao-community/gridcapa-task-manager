e9425cd1f95840e81d0b430fbd5bcb929e8d00c2
Vincent BOCHET <vincent.bochet@rte-france.com>
2025-07-31 09:55:01 +0200

    Replace SockJS with standard websockets

--- gridcapa-task-manager-app/src/main/java/com/farao_community/farao/gridcapa/task_manager/app/configuration/WebsocketConfig.java
+++ gridcapa-task-manager-app/src/main/java/com/farao_community/farao/gridcapa/task_manager/app/configuration/WebsocketConfig.java
@@ -3,6 +3,7 @@ package com.farao_community.farao.gridcapa.task_manager.app.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,6 +14,12 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Value("${stomp.allowed-origin}")
     private String[] allowedOrigin;
+
+    @Value("${stomp.heartbeat-client}")
+    private long heartbeatClient;
+
+    @Value("${stomp.heartbeat-server}")
+    private long heartbeatServer;
 
     @Value("${stomp.starting-ws-endpoint}")
     private String startingEndpoint;
@@ -25,14 +32,21 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        final ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(1);
+        taskScheduler.setThreadNamePrefix("ws-heartbeat-thread-");
+        taskScheduler.initialize();
+
         // These are endpoints the client can subscribes to.
-        config.enableSimpleBroker(notify);
+        config.enableSimpleBroker(notify)
+                .setHeartbeatValue(new long[]{heartbeatServer, heartbeatClient})
+                .setTaskScheduler(taskScheduler);
         config.setApplicationDestinationPrefixes(receiver);
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint(startingEndpoint).setAllowedOriginPatterns(allowedOrigin).withSockJS();
+        registry.addEndpoint(startingEndpoint).setAllowedOriginPatterns(allowedOrigin);
     }
 
     public String getNotify() {
